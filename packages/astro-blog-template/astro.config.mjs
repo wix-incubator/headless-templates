5b11f121d9efbdc07309408b10a7b1cbe448e06e
nitayneeman <nitayneeman@users.noreply.github.com>
2025-01-21 17:30:12 +0200

    feat: integrate ricos

--- packages/astro-blog-template/astro.config.mjs
+++ packages/astro-blog-template/astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
