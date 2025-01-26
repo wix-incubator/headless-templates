34c3b8c798e7404af3788cb97ca96d317fcb3849
nitayneeman <nitayneeman@users.noreply.github.com>
2025-01-26 14:49:29 +0200

    feat: implement RicosViewer

--- packages/astro-ricos/src/utils/ricosToHtml.ts
+++ packages/astro-ricos/src/utils/ricosToHtml.ts
@@ -7,27 +7,60 @@ import {
 } from "../types";
 import { media } from "@wix/sdk";
 
-const wrapWithTag = (content: string, tag: string, attributes = "") =>
-  `<${tag}${attributes ? ` ${attributes}` : ""}>${content ?? ""}</${tag}>`;
+const objectToAttributes = (attributes: Record<string, string>): string =>
+  Object.entries(attributes)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join(" ");
+
+const objectToStyle = (style: Record<string, string>): string =>
+  Object.entries(style)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join("; ");
+
+const wrapWithTag = ({
+  tag,
+  attributes = {},
+  children = "",
+  style = {},
+}: {
+  tag: string;
+  children?: string;
+  attributes?: Record<string, string>;
+  style?: Record<string, string>;
+}) => {
+  const attributesString = Object.keys(attributes).length
+    ? ` ${objectToAttributes(attributes)}`
+    : "";
+  const styleString = Object.keys(style).length
+    ? ` style="${objectToStyle(style)}"`
+    : "";
+  return `<${tag}${attributesString}${styleString}>${children}</${tag}>`;
+};
 
 const applyLinkDecoration = (text: string, linkData: any) => {
   const { url, target, rel } = linkData.link;
   const relAttrs = Object.keys(rel || {}).join(" ");
-  return wrapWithTag(
-    text,
-    "a",
-    `href="${url}" target="${target || "_self"}" rel="${relAttrs}"`
-  );
+  return wrapWithTag({
+    tag: "a",
+    attributes: {
+      href: url,
+      target: target || "_self",
+      rel: relAttrs,
+    },
+    children: text,
+  });
 };
 
 const applyColorDecoration = (text: string, colorData: any) => {
   const { background, foreground } = colorData || {};
   const style = background
-    ? `background-color: ${background}`
+    ? { "background-color": background }
     : foreground
-    ? `color: ${foreground}`
-    : "";
-  return style ? wrapWithTag(text, "span", `style="${style}"`) : text;
+    ? { color: foreground }
+    : {};
+  return Object.keys(style).length
+    ? wrapWithTag({ tag: "span", style, children: text })
+    : text;
 };
 
 const applyDecorations = (
@@ -37,31 +70,42 @@ const applyDecorations = (
   decorations.reduce((result, decoration) => {
     switch (decoration.type) {
       case DecorationType.BOLD:
-        return wrapWithTag(result, "strong");
+        return wrapWithTag({ tag: "strong", children: result });
       case DecorationType.ITALIC:
-        return wrapWithTag(result, "em");
+        return wrapWithTag({ tag: "em", children: result });
       case DecorationType.UNDERLINE:
-        return wrapWithTag(result, "u");
+        return wrapWithTag({ tag: "u", children: result });
       case DecorationType.SPOILER:
-        return wrapWithTag(result, "span", 'class="spoiler"');
+        return wrapWithTag({
+          tag: "span",
+          attributes: { class: "spoiler" },
+          children: result,
+        });
       case DecorationType.ANCHOR:
-        return wrapWithTag(result, "a", `name="${decoration.anchorData.name}"`);
+        return wrapWithTag({
+          tag: "a",
+          attributes: { name: decoration.anchorData.name },
+          children: result,
+        });
       case DecorationType.MENTION:
-        return wrapWithTag(
-          result,
-          "span",
-          `class="mention" data-mention-id="${decoration.mentionData.id}"`
-        );
+        return wrapWithTag({
+          tag: "span",
+          attributes: {
+            class: "mention",
+            "data-mention-id": decoration.mentionData.id,
+          },
+          children: result,
+        });
       case DecorationType.LINK:
         return applyLinkDecoration(result, decoration.linkData);
       case DecorationType.COLOR:
         return applyColorDecoration(result, decoration.colorData);
       case DecorationType.FONT_SIZE:
-        return wrapWithTag(
-          result,
-          "span",
-          `style="font-size: ${decoration.fontSizeData.size}px"`
-        );
+        return wrapWithTag({
+          tag: "span",
+          style: { "font-size": `${decoration.fontSizeData.size}px` },
+          children: result,
+        });
       default:
         return result;
     }
@@ -71,26 +115,27 @@ const renderTextNode = (node: RicosNode) =>
   applyDecorations(node.textData.text, node.textData.decorations);
 
 const renderHeadingNode = (node: RicosNode) =>
-  wrapWithTag(
-    renderRicosNode(node.nodes!),
-    `h${node.headingData.level || 1}`,
-    `style="text-align: ${
-      node.headingData?.textStyle?.textAlignment || RicosAlignment.LEFT
-    }"`
-  );
+  wrapWithTag({
+    tag: `h${node.headingData.level || 1}`,
+    style: {
+      textAlign:
+        node.headingData?.textStyle?.textAlignment || RicosAlignment.LEFT,
+    },
+    children: renderRicosNode(node.nodes!),
+  });
 
 const renderParagraphNode = (node: RicosNode) =>
-  wrapWithTag(renderRicosNode(node.nodes!), "p");
+  wrapWithTag({ tag: "p", children: renderRicosNode(node.nodes!) });
 
 const renderBulletedListNode = (node: RicosNode) =>
-  wrapWithTag(
-    renderRicosNode(node.nodes!),
-    "ul",
-    `style="margin-left: ${node.bulletedListData?.indentation || 0}em"`
-  );
+  wrapWithTag({
+    tag: "ul",
+    style: { marginLeft: `${node.bulletedListData?.indentation || 0}em` },
+    children: renderRicosNode(node.nodes!),
+  });
 
 const renderListItemNode = (node: RicosNode) =>
-  wrapWithTag(renderRicosNode(node.nodes!), "li");
+  wrapWithTag({ tag: "li", children: renderRicosNode(node.nodes!) });
 
 const renderImageNode = (node: RicosNode, helpers: any) => {
   const { src, width, height } = node.imageData.image;
@@ -99,12 +144,19 @@ const renderImageNode = (node: RicosNode, helpers: any) => {
   ).url;
   const alignment = node.imageData.containerData.alignment.toLowerCase();
   const caption = renderRicosNode(node.nodes!, helpers);
-  return wrapWithTag(
-    `<img src="${imageUrl}" width="${width}" height="${height}" alt="${node.imageData.altText}" />` +
-      (caption ? wrapWithTag(caption, "div", `class="caption"`) : ""),
-    "div",
-    `style="text-align: ${alignment}"`
-  );
+  return wrapWithTag({
+    tag: "div",
+    style: { textAlign: alignment },
+    children:
+      `<img src="${imageUrl}" width="${width}" height="${height}" alt="${node.imageData.altText}" />` +
+      (caption
+        ? wrapWithTag({
+            tag: "div",
+            attributes: { class: "caption" },
+            children: caption,
+          })
+        : ""),
+  });
 };
 
 const renderTableNode = (node: RicosNode) => {
@@ -116,21 +168,25 @@ const renderTableNode = (node: RicosNode) => {
     )
     .join("");
   const tableRows = renderRicosNode(node.nodes!);
-  return `
-    <table style="width: 100%; border-collapse: collapse;">
-      <colgroup>${colGroup}</colgroup>
-      <tbody>${tableRows}</tbody>
-    </table>`;
+  return wrapWithTag({
+    tag: "table",
+    style: { width: "100%", borderCollapse: "collapse" },
+    children: `<colgroup>${colGroup}</colgroup><tbody>${tableRows}</tbody>`,
+  });
 };
 
 const renderTableRowNode = (node: RicosNode) =>
-  wrapWithTag(renderRicosNode(node.nodes!), "tr");
+  wrapWithTag({ tag: "tr", children: renderRicosNode(node.nodes!) });
 
 const renderTableCellNode = (node: RicosNode) =>
-  wrapWithTag(renderRicosNode(node.nodes!), "td");
+  wrapWithTag({ tag: "td", children: renderRicosNode(node.nodes!) });
 
 const renderCaptionNode = (node: RicosNode) =>
-  wrapWithTag(renderRicosNode(node.nodes!), "div", `class="caption"`);
+  wrapWithTag({
+    tag: "div",
+    attributes: { class: "caption" },
+    children: renderRicosNode(node.nodes!),
+  });
 
 const renderRicosNode = (nodes: RicosNode[], helpers?: any): string =>
   nodes
